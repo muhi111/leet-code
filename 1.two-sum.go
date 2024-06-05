bfe3785ab344f75aadec70715ff03e286673bb28
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-06-05 15:45:28 +0900

    1

---
+++ 1.two-sum.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+// @lc code=end
